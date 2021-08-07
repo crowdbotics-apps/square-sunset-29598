5ae8a722f91b1110c1416d49aca9ebd02290d02c
Crowdbotics <team@crowdbotics.com>
2021-08-07 09:20:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "square_sunset_29598Identifier",
+  url: "https://square_sunset_29598Identifier.botics.co",
+  api: "https://square_sunset_29598Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
